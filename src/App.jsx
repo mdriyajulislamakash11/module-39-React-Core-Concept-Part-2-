33539a61ed941e06bbca15505072eaa3e4cb2d2e
Akash <hafejmohammadakash@gmail.com>
2025-04-07 08:17:24 -0600

    onClick event er onk gulo rup dekhano holo

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+  function hanleClick() {
+    alert("button clicked");
+  }
+
+  const handleclick2 = () => {
+    alert("click 2");
+  };
+
+  const addToFive = (num) => {
+   alert (num)
+    
+  };
+
+  return (
+    <>
+      <h2>React Core Concept 2</h2>
+      <button onClick={hanleClick}>Click Me</button>
+
+      <button onClick={handleclick2}>Click Me 2</button>
+
+      <button
+        onClick={() => {
+          alert("alert to third");
+        }}
+      >
+        third
+      </button>
+
+      <button onClick={() => addToFive(3)}>addToFive</button>
+    </>
+  );
+}
+
+export default App;
